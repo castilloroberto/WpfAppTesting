e4122393dbc611867e8656997d5f647440fecf09
robertocastillo <robertocastillo945@gmail.com>
2021-05-25 15:18:29 -0600

    Agregar archivos de proyecto.

---
+++ WpfAppTesting/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Person> listPeople = new List<Person>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            string id = (listPeople.Count() + 1).ToString(); 
+            Person person = new Person(id,txt_name.Text,txt_lastname.Text,txt_email.Text,txt_brithday.Text,txt_gender.Text);
+            listPeople.Add(person);
+            UpdateDatagrid();
+        }
+
+
+        private void UpdateDatagrid()
+        {
+
+            foreach (Person person in listPeople)
+            {
+                dgv_people.Items.Add(person);
+
+            }
+        }
+
+    }
+}
---
+++ WpfAppTesting/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace WpfAppTesting
+{
+    public class Person
+    {
+        //get obtener
+        //set establecer
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Birthday { get; set; }
+
+        public string Gender { get; set; }
+
+
+
+        //constructor
+
+        public Person(string _id,string _name,string _lastname,string _email,string _birthday,string _gender)
+        {
+            ID = _id;
+            Name = _name;
+            Lastname = _lastname;
+            Email = _email;
+            Birthday = _birthday;
+            Gender = _gender;
+
+        }
+
+
+
+    }
+}
